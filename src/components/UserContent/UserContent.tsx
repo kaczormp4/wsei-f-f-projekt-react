452eea3429ec22d8a556c2715b115482590bddf5
kaczormp4 <kaczkaoryginal@gmail.com>
2021-05-31 19:55:50 +0200

    user [panel

--- src/components/UserContent/UserContent.tsx
+++ src/components/UserContent/UserContent.tsx
@@ -17,6 +17,8 @@ const UserInfo = styled.div`
     width: 90%;
     height:300px;
     display:flex;
+    box-sizing: border-box;
+    padding-right:50px;
 `;
 const PhotoBox = styled.div`
     width: auto;
@@ -60,28 +62,99 @@ const About = styled.div`
 `;
 const PanelInformation = styled.div`
     width: 90%;
-    height:500px;
     background-color:white;
-`;
-const SerwicesAndProjects = styled.div`
-    width: 90%;
-    height:500px;
-    background-color:#608a59;
+    text-align:left;
+    box-sizing: border-box;
+    padding-top: 40px;
+    padding: 50px;
+    border-bottom: 1px solid lightgrey;
+    h1 {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    p {
+        color:grey;
+    }
+    div {
+        display: flex;
+        background-color: #e6f0f3;
+        padding: 10px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        b {
+            margin-right: 5px;
+            margin-left: 5px; 
+        }
+        span {
+            margin-right: 15px; 
+            margin-left: 15px; 
+        }
+    }
 `;
 const Proposals = styled.div`
     width: 90%;
-    height:500px;
-    background-color:#b7c3b5;
+    background-color:white;
+    text-align:left;
+    box-sizing: border-box;
+    padding-top: 40px;
+    padding: 50px;
+    border-bottom: 1px solid lightgrey;
+
+    h1 {
+        margin: 0;
+    }
+    table {
+        width: 100%;
+    }
+    div {
+        color: #86e3ff;
+        width: 100%;
+        text-align: right;
+        padding-top: 40px;
+        font-size: 1.4rem;
+        font-weight: bold;
+    }
 `;
 const InternalRewiews = styled.div`
     width: 90%;
-    height:500px;
-    background-color:#194a79;
+    background-color:white;
+    text-align:left;
+    box-sizing: border-box;
+    padding-top: 40px;
+    padding: 50px;
+    border-bottom: 1px solid lightgrey;
+
+    h1 {
+        margin: 0;
+    }
+    table {
+        width: 100%;
+    }
+    div {
+        color: #86e3ff;
+        width: 100%;
+        text-align: right;
+        padding-top: 40px;
+        font-size: 1.4rem;
+        font-weight: bold;
+    }
 `;
 const AmountOfFes = styled.div`
-width: 90%;
-height:500px;
-background-color:#199c1f;
+    width: 90%;
+    background-color:white;
+    text-align:left;
+    box-sizing: border-box;
+    padding-top: 40px;
+    padding: 50px;
+    border-bottom: 1px solid lightgrey;
+
+    h1 {
+        margin: 0;
+    }
+    table {
+        width: 100%;
+    }
+
 `;
 function UserContent(){
     return (
@@ -121,19 +194,122 @@ function UserContent(){
                 <span>Merges and acquisition</span>
             </About>
             <PanelInformation>
-
-            </PanelInformation>
-            <SerwicesAndProjects>
-
-            </SerwicesAndProjects>       
+                <h1>Panel information</h1>
+                <p>Hourly free</p>
+                <span>610E/hour (negociated)</span>
+                <p>Terms and Conditions</p>
+                <span>Monthy 10kE Retainer - see with Jeanny Smith</span>
+                <div>ICO Attachment Lorem ipsum</div>
+                <h1>Service and projects</h1>
+                <p>Corporate M and A and international acquisitions</p>
+                <h1>Internal correspondants</h1>
+                <div>
+                    [PHOTO] <b>First Name </b> <b>Last Name </b><span>[IMG] Message</span><span>[IMG] Profile</span>
+                </div>
+                <div>
+                    [PHOTO] <b>First Name </b> <b>Last Name </b><span>[IMG] Message</span><span>[IMG] Profile</span>
+                </div>
+            </PanelInformation>      
             <Proposals>
-
+                <h1>Proposals</h1>
+                <table>
+                    <tr>
+                        <td><h2>Name</h2></td>
+                        <td><h2>Entity</h2></td>
+                        <td><h2>Location</h2></td>
+                        <td><h2>Expertise</h2></td>
+                        <td><h2>Date</h2></td>
+                        <td><h2>Firm</h2></td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                </table>
+                <div> SEE MORE PROPOSALS</div>
             </Proposals>      
             <InternalRewiews>
-
+                <h1>Internal Reviews</h1>
+                <table>
+                    <tr>
+                        <td><h2>Name</h2></td>
+                        <td><h2>Entity</h2></td>
+                        <td><h2>Location</h2></td>
+                        <td><h2>Expertise</h2></td>
+                        <td><h2>Date</h2></td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                </table>
+                <div> SEE MORE REWIEWS</div>
             </InternalRewiews>   
             <AmountOfFes>
-
+            <h1>Internal Reviews</h1>
+                <table>
+                    <tr>
+                        <td><h2>Year</h2></td>
+                        <td><h2>CostCenter</h2></td>
+                        <td><h2>Total Amount</h2></td>
+                        <td><h2>Law Firm</h2></td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem </td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                    </tr>
+                </table>
             </AmountOfFes> 
         </UserContentContainer>
     )
