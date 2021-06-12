e07a0ac3d6b909f82823cad5616b5ed7ee058f2e
kaczormp4 <kaczkaoryginal@gmail.com>
2021-06-12 20:35:40 +0200

    .

---
+++ src/actions/PhotosAcrion.tsx
@@ -0,0 +1,28 @@
+import { Dispatch } from 'redux';
+
+interface IPhoto {
+    photo: string | undefined;
+    albumId: number,
+    id: number,
+    thumbnailUrl: string,
+    title: string,
+    url: string
+}
+// interface IPhotoTypes {
+//     GET_PHOTO: {
+//         photoList: IPhoto[];
+//     };
+// }
+const GET_PHOTO = 'GET_PHOTO';
+
+export const getPhoto = (): Promise<IPhoto[]> => ((dispatch: Dispatch) => {
+
+    return fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then((photoList: IPhoto[]) => {
+            dispatch({
+                type: GET_PHOTO,
+                photoList
+            })
+        })
+}) as any;
--- src/reducers/PublicationReducer.tsx
+++ src/reducers/PublicationReducer.tsx
@@ -2,6 +2,8 @@ const publications = (state = ['ss'], action:any) => {
     switch(action.type) {
         case 'TEST':
             return [];
+        case 'GET_PHOTO':
+            return [];
         default:
             return state;
         }
